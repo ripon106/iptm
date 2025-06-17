6d9bc683f716cec23c2a1bedea9bc2b4d5601434
ripon106 <riponsheikh106@gmail.com>
2025-06-17 12:25:08 +0600

    bodyparser

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
+const bodyParser = require('body-parser');
+
 const app = express()
 const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
-
+app.use(bodyParser.json());
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1rrkbzk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
